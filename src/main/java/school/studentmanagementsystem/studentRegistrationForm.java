aff0a55fd2cd87634824abdd21496a52229ff781
Yimnai Nerus Zaumu <neruszaumuy@gmail.com>
2021-12-12 12:38:46 +0100

    Sign-in page in progress

---
+++ src/main/java/school/studentmanagementsystem/studentRegistrationForm.java
@@ -0,0 +1,25 @@
+package school.studentmanagementsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class studentRegistrationForm extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(studentRegistrationForm.class.getResource("studentRegistration-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add("studentRegistrationStyles.css");
+        stage.setTitle("Registration Form");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
